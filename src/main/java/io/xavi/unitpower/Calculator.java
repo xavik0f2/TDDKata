b94cddf3b26b31a188bc57fd81902cd478eacd12
Xavier Llanga <xavierllanga@hotmail.com>
2018-12-09 20:34:02 +0100

    Refactor. Corregidos nombres

--- src/main/java/io/xavi/unitpower/Calculator.java
+++ src/main/java/io/xavi/unitpower/Calculator.java
@@ -2,14 +2,14 @@ package io.xavi.unitpower;
 
 public class Calculator {
 
-    public int add(String input) {
-        if (input == "") return 0;
-        if (!input.contains(",")) return Integer.parseInt(input);
-        String[] inputSplitted = input.split(",");
+    public int add(String inputNumbers) {
+        if (inputNumbers == "") return 0;
+        if (!inputNumbers.contains(",")) return Integer.parseInt(inputNumbers);
+        String[] inputSplitted = inputNumbers.split(",");
 
         int sum=0;
-        for (String sumando:inputSplitted)
-            sum += Integer.parseInt(sumando);
+        for (String number :inputSplitted)
+            sum += Integer.parseInt(number);
 
         return sum;
     }
