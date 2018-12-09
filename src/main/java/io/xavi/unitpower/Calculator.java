957cb8c3d7b63ff6173f53d27ed7f14dc943121b
Xavier Llanga <xavierllanga@hotmail.com>
2018-12-09 20:01:39 +0100

    Pasan correctamente los tres tests

--- src/main/java/io/xavi/unitpower/Calculator.java
+++ src/main/java/io/xavi/unitpower/Calculator.java
@@ -3,8 +3,9 @@ package io.xavi.unitpower;
 public class Calculator {
 
     public int add(String input) {
-        if(input == "") return 0;
+        if (input == "") return 0;
+        if (!input.contains(",")) return Integer.parseInt(input);
         String[] inputSplitted = input.split(",");
-        return Integer.parseInt(inputSplitted[0])+Integer.parseInt(inputSplitted[1]);
+        return Integer.parseInt(inputSplitted[0]) + Integer.parseInt(inputSplitted[1]);
     }
 }
