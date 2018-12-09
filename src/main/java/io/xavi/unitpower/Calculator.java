fbd27e073f4d539b45e565a8c940e3515451e563
Xavier Llanga <xavierllanga@hotmail.com>
2018-12-09 21:31:35 +0100

    Se implementa nuevo delimitador y refactoriza. Tests OKs

--- src/main/java/io/xavi/unitpower/Calculator.java
+++ src/main/java/io/xavi/unitpower/Calculator.java
@@ -2,15 +2,40 @@ package io.xavi.unitpower;
 
 public class Calculator {
 
-    public int add(String inputNumbers) {
-        if (inputNumbers == "") return 0;
-        if (!inputNumbers.contains(",")) return Integer.parseInt(inputNumbers);
-        String[] inputNumbersSplitted = inputNumbers.split(",");
+    public int add(String input) {
 
-        int sum=0;
-        for (String number :inputNumbersSplitted)
+        String[] delimiters = {",", "\n"};
+
+        if (inputIsNull(input)) return 0;
+
+        if (inputWithoutDelimiter(delimiters, input)) return Integer.parseInt(input);
+
+        String[] numbers = splitNumbers(delimiters, input);
+
+        return sum(numbers);
+
+    }
+
+    private boolean inputWithoutDelimiter(String[] delimiters, String input) {
+        return (!input.contains(delimiters[0]) && !input.contains(delimiters[1]));
+    }
+
+    private String[] splitNumbers(String[] delimiters, String input) {
+        String regexDelimiters = delimiters[0] + "|" + delimiters[1];
+        return input.split(regexDelimiters);
+
+    }
+
+    private int sum(String[] inputs) {
+        int sum = 0;
+        for (String number : inputs)
             sum += Integer.parseInt(number);
 
         return sum;
     }
+
+    private boolean inputIsNull(String input) {
+        return input == "";
+    }
+
 }
